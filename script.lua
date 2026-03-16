bd734799b5d59af72f92e66db7c04e390baaaaff
repagithhdo <taylorserren02@gmail.com>
2026-03-16 02:25:38 -0400

    Spawn tasks to load scripts concurrently
    Added additional task to load a script from Pastebin.

--- script.lua
+++ script.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ef18981bd4d52c2ae04feca4bf5bd94a.lua"))()
+task.spawn(function()
+ loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ef18981bd4d52c2ae04feca4bf5bd94a.lua"))()
+end)
+
+task.spawn(function()
+   loadstring(game:HttpGet("https://pastebin.com/raw/8CrdCWVC"))()
+end)
